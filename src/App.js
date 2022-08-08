c5af3a665fb6d2d7e0d521c1b6b88f3578e7d419
Manikandan <manikandan.thangarasu@solverminds.com>
2022-08-08 20:26:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './images/vyoobam_logo.png';
+import './App.css';
+
+function App() {
+  //http://jsonplaceholder.typicode.com/posts--> posts
+  return (
+    <div className="App">
+      <header>
+        {/* <img src={logo} alt="vyoobam_logo" /> */}
+        <p>Welcome to the Vyoobam Web Solutions</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
